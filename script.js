41e1ca14d4daf55997e9ba84b0fe8857eade806b
SERINE <serinebagdasaryan5@gmail.com>
2019-11-04 13:22:32 +0400

    with 2 players

--- script.js
+++ script.js
@@ -1,29 +1,39 @@
 let size = prompt("Enter board size", 5);
 if(size > 100){
     alert("Enter a number less than 100");
-}else if(size<=3){
-    alert("enter big number");
+}
+else if(size <=3){
+    alert("Enter big number");
 }
 else {
     window.onload = function () {
+        let buttons = document.getElementsByTagName('td');
+        console.log(buttons);
         let turn;
-      document.getElementById("choose").addEventListener('click',function () {
-          if($("input[type='radio'].radioBtnClass").is(':checked')) {
-              turn = $("input[type='radio'].radioBtnClass:checked").val();
-              document.getElementById('tictactoe').style.display = "block";
-              document.getElementById('players').style.display = "none";
-          }
-           if(!$("input[type='radio'].radioBtnClass").is(':checked')) {
-             alert('choose one of  players');
-               document.getElementById('tictactoe').style.display = "none";
-
-          }
-
-
-        })
-
+        let computer;
+        let user;
+        function randomNumber() {
+            let item = size*size;
+            console.log(item);
+            console.log(Math.floor(Math.random() * (item)))
+            return Math.floor(Math.random() * (item));
+        }
+        randomNumber();
+        $(".checkBox").click(function() {
+            console.log("checked");
+            if($(this).is(":checked")) {
+                turn = $(this).val();
+                user = turn;
+                // computer= turn;
+                computer = (user == 'X') ? 'O' : 'X';
+                console.log(turn + " " + user + " " + computer);
+                document.getElementById('tictactoe').style.display = "block";
+                document.getElementById('players').style.display = "none";
+            }
+        });
+        let board;
         let squares = [],
-            empty = "&nbsp;",
+            empty = "",
             score,
             moves,
             newGame = function () {
@@ -36,44 +46,87 @@ else {
                 squares.forEach(function (square) {
                     square.html(empty);
                 });
-            }
+
+
+        }
+
+
         getWinner = function (clicked) {
             let xOrO = clicked[0].className.split(/\s+/);
             for (let i = 0; i < xOrO.length; i++) {
                 let newClass = '.' + xOrO[i];
                 if ($('#tictactoe').find(newClass + ':contains(' + turn + ')').length == size) {
+
                     return true;
                 }
             }
             return false;
         }
+
+
         set = function () {
+            let item = size* size;
             if ($(this).html() !== empty) {
                 return;
             }
             $(this).html(turn);
             moves += 1;
             score[turn] += $(this)[0].indicator;
-            console.log(score[turn]);
+            // console.log(score[turn]);
             if (getWinner($(this))) {
                 alert(turn + "winns!");
                 newGame();
             } else if (moves === size * size) {
-                alert('x and o game');
+                alert('\n' +
+                    'the game ended in a draw');
 
             } else {
-                turn = turn === "X" ? "O" : "X";
+                for (let i=0; i < item; i++) {
+                    let random = randomNumber();
+                    if (buttons[random].innerHTML === "X" || buttons[random].innerHTML === "O") {
+                        continue;
+                    }
+                    else {
+                        buttons[random].innerHTML = computer;
+                        break;
+                    }
+                }
+
+            }
+            draw();
+
+        }
+
+        function draw() {
+        let items= size*size;
+            let counter = 0;
+            while (counter < items) {
+
+                if (buttons[counter].innerHTML === "X" || buttons[counter].innerHTML === "O") {
+                    console.log(buttons[counter].innerHTML)
+                    console.log("has an X or Y")
+                    counter += 1;
+                }
+                else {
+                    console.log("there is nothing there")
+                    return;
+                }
+                if (counter === items-1) {
+                    alert("its a draw")
+                    $("td").text("")
+                }
             }
         }
 
         playGame = function () {
-            let board = $("<table border=1 cellspacing=0>"),
+            board = $("<table border=1 cellspacing=0>"),
                 indicator = 1;
+               let cell;
             for (let i = 0; i < size; i += 1) {
                 let row = $("<tr>");
                 board.append(row);
                 for (let j = 0; j < size; j += 1) {
-                    let cell = $("<td height=120 width= 120 align=center valign=center></td>");
+                    cell = $("<td height=120 width= 120 align=center valign=center></td>");
                     cell.addClass('col' + j);
                     cell.addClass('row' + i);
                     if (i == j) {
@@ -84,10 +137,14 @@ else {
                     }
                     cell[0].indicator = indicator;
                     cell.click(set);
+
                     row.append(cell);
                     squares.push(cell);
+
                     indicator += indicator;
                 }
+                // console.log(cell, row);
+
             }
             $(document.getElementById("tictactoe") || document.body).append(board);
             newGame();
